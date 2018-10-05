7de09244846a622131fbe00f5d91e6739523c61a
Jakub Majak <jakub.majak@firma.seznam.cz>
2018-10-05 08:21:16 +0200

    fix barvy svg ikon v panelu

--- src/NodeContainer.js
+++ src/NodeContainer.js
@@ -214,7 +214,12 @@ export default class NodeContainer {
                 node.setAttribute("width", this.bounds.width);
                 node.setAttribute("height", this.bounds.height);
                 if ("fill" in style && style.fill != "") {
-                    node.setAttribute("fill", style.fill);
+                    if (style.fill == "rgb(0, 0, 0)") {
+                        node.setAttribute("fill", style.color);
+                    }
+                    else {
+                        node.setAttribute("fill", style.fill);
+                    }
                 }
             }
         }
