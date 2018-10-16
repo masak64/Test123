fad25ef42066f2abad9f110397155880315a2efa
Jakub Majak <jakub.majak@firma.seznam.cz>
2018-10-16 14:04:00 +0200

    upravy pro tisk

--- src/NodeContainer.js
+++ src/NodeContainer.js
@@ -214,7 +214,7 @@ export default class NodeContainer {
                 node.setAttribute("width", this.bounds.width);
                 node.setAttribute("height", this.bounds.height);
                 if ("fill" in style && style.fill != "") {
-                    if (style.fill == "rgb(0, 0, 0)") {
+                    if (style.fill == "rgb(0, 0, 0)" || style.fill.indexOf("rgb") < 0) {
                         node.setAttribute("fill", style.color);
                     }
                     else {
